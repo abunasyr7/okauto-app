4cffebaad2eba26f67d11b5f68fb3ea0817dec9e
Abunasyr <abunasyr1@gmail.com>
2023-08-10 16:59:15 +0600

    feat: add color to icons

--- src/components/Icons.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Svg from 'react-native-svg';
-
-type IconProps = {
-  color?: string;
-  size?: number;
-};
--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -17,18 +17,48 @@ export const BottomTabsNavigator: React.FC = () => {
         options={{
           tabBarIcon: ({focused}) => (
             <Image
-              source={
-                focused
-                  ? require('../../assets/images/penalties-tab-icon-active.png')
-                  : require('../../assets/images/penalties-tab-icon.png')
-              }
+              source={require('../../assets/images/penalties-tab-icon.png')}
+              tintColor={focused ? '#0588ee' : '#bdbdbd'}
             />
           ),
         }}
       />
-      <BottomTabs.Screen name="Страховка" component={Insurance} />
-      <BottomTabs.Screen name="Уведомления" component={Notifications} />
-      <BottomTabs.Screen name="Настройки" component={Settings} />
+      <BottomTabs.Screen
+        name="Страховка"
+        component={Insurance}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={require('../../assets/images/shield-tab-bar-icon.png')}
+              tintColor={focused ? '#0588ee' : '#bdbdbd'}
+            />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="Уведомления"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={require('../../assets/images/notifications-tab-icon.png')}
+              tintColor={focused ? '#0588ee' : '#bdbdbd'}
+            />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="Настройки"
+        component={Settings}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={require('../../assets/images/settings-tab-icon.png')}
+              tintColor={focused ? '#0588ee' : '#bdbdbd'}
+            />
+          ),
+        }}
+      />
     </BottomTabs.Navigator>
   );
 };
